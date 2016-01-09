a6b36d2b6f85a21f27f53a2a96d6b6fc7303b912
dhojpj <dhojpj@gmail.com>
2016-01-08 20:11:16 -0800

    firefox edit today

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -4,13 +4,10 @@ using namespace std;
 
 int main()
 {
-    // added this comment
-    // will this in browser work?
-    
-    
     // this edit was on my iPad using Textastic (did it work???)
     // because I did not modify it directly (Working Copy app)
     
+    // this is Firefox
     
     cout << "Hello World!" << endl;
     return 0;
