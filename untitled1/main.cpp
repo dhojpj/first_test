2214c226060e347cc8b7a4509e6209f47f1a2d86
Daniel Ho <dhojpj@gmail.com>
2016-01-08 20:21:16 -0800

    edited with iPad

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main()
 {
     // this edit was done on Qt Creator on PC
-
-
-
     cout << "Hello Qt Creator!" << endl;
+    
+    // this edit was done using Textastic with Working Copy as the repository
+    cout << "Hello Textastic!" << endl; // for some reason Working Copy cannot add text to the files -- it's purely a repository (i.e. it just stores files)
     return 0;
 }
 
