56e032aeee062fd7600665e64f3753919fce32c5
dhojpj <dhojpj@gmail.com>
2016-01-08 20:27:24 -0800

    iPad edit again, and renamed my username

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -8,7 +8,9 @@ int main()
     cout << "Hello Qt Creator!" << endl;
     
     // this edit was done using Textastic with Working Copy as the repository
-    cout << "Hello Textastic!" << endl; // for some reason Working Copy cannot add text to the files -- it's purely a repository (i.e. it just stores files)
+    cout << "Hello Working Copy!\nTextastic!" << endl;
+    // for some reason Working Copy cannot add text to the files -- it's purely a repository (i.e. it just stores files)
+    
     return 0;
 }
 
