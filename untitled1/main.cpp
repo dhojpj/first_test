3e0d04cf864512b8b5942cc07839ac095f88a623
Daniel Ho <dhojpj@gmail.com>
2016-01-08 20:01:12 -0800

    edited using Textastic and committed with Working Copy

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -6,6 +6,12 @@ int main()
 {
     // added this comment
     // will this in browser work?
+    
+    
+    // this edit was on my iPad using Textastic (did it work???)
+    // because I did not modify it directly (Working Copy app)
+    
+    
     cout << "Hello World!" << endl;
     return 0;
 }
