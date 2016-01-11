0437cea1436f435fcf5592059cdda5baedcc1dbc
dhojpj <dhojpj@gmail.com>
2016-01-10 20:55:38 -0800

    updated on PC

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -6,21 +6,24 @@ int main()
 {
     // this edit was done on Qt Creator on PC
     cout << "Hello Qt Creator!" << endl;
-   
-       // this edit was on Working Copy!
-    cout << "Hello Working Copy!" << endl;   
+
+    // this edit was on Working Copy!
+    cout << "Hello Working Copy!" << endl;
     
     // this edit was done using Textastic with Working Copy as the repository
     cout << "Hello Textastic!" << endl;
     
     // this edit was done on iPad Textastic
-    cout << "Hello again!" << endl;
+    cout << "Hello iPad!" << endl;
 
     // while this edit was done on my Mac
     cout << "Hello Mac World!" << endl;
 
-    // while this edit was done on my iPhone 
+    // while this edit was done on my iPhone
     cout << "Hello iPhone World!" << endl;
+
+    // back to PC
+    cout << "Hello PC World!" << endl << endl;
 
     return 0;
 }
